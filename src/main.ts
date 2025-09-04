6dd6306b6ce622deff534dce6c5a5777c7d732ff
Victor Hernandez <victorhe92@gmail.com>
2025-09-03 20:08:57 -0400

    Configuración inicial

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API AirConnect backend')
+    .setDescription('Esta api describe la plataforma de vuelos de Air Connect')
+    .setVersion('1.0.0')
+    .addTag('app-airconnect-api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
